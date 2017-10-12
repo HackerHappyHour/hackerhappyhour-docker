9f5551747509d2766dca0ae62f9410a2281edd29
Brian Vanderbusch <brian.vanderbusch@gmail.com>
2017-10-11 22:39:47 -0500

    fix lint and syntax errors

---
+++ manifests/repository.pp
@@ -0,0 +1,21 @@
+# == Class: docker::repository
+#
+#
+class docker::repository (
+  $docker_repository,
+  $manage_repos = $docker::manage_package_repository
+) {
+
+  if $manage_repos {
+    case $::osfamily {
+      'Debian': {
+        include apt
+        create_resources('apt::source', $docker_repository)
+      }
+      'RedHat': {
+        create_resources('yumrepo', $docker_repository)
+      }
+      default: {}
+    }
+  }
+}
