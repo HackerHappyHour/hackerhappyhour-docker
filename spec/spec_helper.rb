845702a85c6103f2d490cfb9e16c64f51c10e10a
Brian Vanderbusch <brian.vanderbusch@gmail.com>
2017-10-12 00:30:56 -0500

    update spec and fixtures config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,12 @@
+require 'rspec-puppet'
 require 'puppetlabs_spec_helper/module_spec_helper'
+
+fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
+RSpec.configure do |c|
+  c.hiera_config = File.join(fixture_path, 'modules/docker/hiera.yaml')
+  c.module_path = File.join(fixture_path, 'modules')
+  c.setup_fixtures = true
+end
 
 RSpec::Matchers.define :require_string_for do |property|
   match do |type_class|
